e3334ce3b2641d6aa5c0cd5d9086ef740db98709
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-11-24 09:16:09 +0100

    Uppgift 1

---
+++ Olympiaden Uppgift 1/Olympiaden Uppgift 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace uppgift_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket ord vill du upprepa?");
+            string ord = Console.ReadLine();
+            Console.WriteLine("Hur många gånger vill du upprepa det?");
+            int antal = int.Parse(Console.ReadLine());
+            if ( antal < 10 && ord.Length < 10 ) 
+            {
+                for (int i = 0; i < antal; i++)
+                {
+                    Console.Write(ord);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ordet kan inte upprepas fler än 9 gånger eller vara längre än 9 bokstäver");
+            }
+            Console.Read();
+        }
+    }
+}
