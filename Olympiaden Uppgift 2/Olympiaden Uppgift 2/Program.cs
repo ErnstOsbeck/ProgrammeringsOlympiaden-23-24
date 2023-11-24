dbb82887c0d52694819bc18944681d529665a90a
ErnstOsbeck <143172129+ErnstOsbeck@users.noreply.github.com>
2023-11-24 10:26:47 +0100

    Uppgift 2

---
+++ Olympiaden Uppgift 2/Olympiaden Uppgift 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace uppgift_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in N");
+            int N = int.Parse(Console.ReadLine());
+            int antal = 0;
+            for (int i = 1; i < N; i++)
+            {
+                int a = i;
+                int b = i + 1;
+                int c = i + 2;
+                int d = a * b * c;
+                if (d < N)
+                {
+                    antal++;
+                }
+            }
+            Console.WriteLine(antal);
+            Console.Read();
+        }
+    }
+}
